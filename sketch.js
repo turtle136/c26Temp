722bddb972a266d6945316dfa333a8fe0716bf67
turtle136 <duckducknone@gmail.com>
2021-04-07 19:40:18 -0500

    command

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var platform;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
-
+        //hi
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
